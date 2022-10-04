3beacb21febb9119c73bb1ab466a78d897a342f2
EmilioAcostaG <103851807+EmilioAcostaG@users.noreply.github.com>
2022-10-03 23:46:43 -0400

    updated server.js

--- Develop/server.js
+++ Develop/server.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const path = require("path");
-const dbData = require("./db/db.json")
+const {notes} = require("./db/db.json")
+const uuid = require("uuid");
 const fs = require("fs");
 
 const app = express();
@@ -10,32 +11,48 @@ const PORT = process.env.PORT || 3001;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(express.static("public"));
+
+// GET Route for index page
+router.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/index.html"))
+});
 
 // GET Route for notes page
 router.get("/notes", (req, res) => {
-  res.sendFile(path.join(__dirname, "public/notes.html"))
+  res.sendFile(path.join(__dirname, "/public/notes.html"))
 });
 
-router.get("api/notes", (req, res) => {
-  res.json(dbData)
+
+// Routes for API
+router.get("/api/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "/db/db.json"))
+});
+
+// Post adds notes to db.json
+router.post("/api/notes", (req, res) => {
+
+  req.body.id = uuid()
+  const notes = JSON.parse(fs.readFileSync("./db/db/json"));
+  const newNotes = req.body;
+  newNotes.id = uuid.v4();
+  notes.push(newNotes);
+  fs.writeFileSync("./db/db.json", JSON.stringify(notes))
+  res.json(notes);
+});
+
+// Delete removes notes from db.json
+router.delete("/api/notes/:id", (req, res) => {
+  const notes = JSON.parse(fs.readFileSync(".db/db.json"));
+  const removeNote = notes.filter((remove) => remove.id !== req.params.id);
+  fs.writeFileSync("./db/db.json", JSON.stringify(removeNote));
+  res.json(removeNote)
 })
 
-// GET Route for index page
-router.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "public/index.html"))
-})
 
-app.post("/api/notes", (req, res) =>
-  fs.readFile(__dirname + "/db/db.json", "utf8", (err, data) => {
-    if (err) {
-      console.error(err);
-    } else {
-      
-    }
-  } )
-)
+router.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/index.html"))
+});
 
 
 app.listen(PORT, () =>
